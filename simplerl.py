3aea84053ed6b60d1255f55dbc545096ad8a26fb
Sebastian Uribe <suribe@gmail.com>
2024-09-09 13:02:53 +0200

    GridEnvironment now takes start and end positions as parameters

--- simplerl.py
+++ simplerl.py
@@ -6,6 +6,7 @@ from rl import *
 
 @dataclass
 class Direction(Enum):
+    """ Simple representation of a direction for movement in the grid """
     Up = (0, -1)
     Down = (0, 1)
     Left = (-1, 0)
@@ -103,17 +104,17 @@ class GridEnvironment(Environment):
     state:GridState
     grid:Grid
 
-    def __init__(self, grid:Grid):
+    def __init__(self, grid:Grid, startPosition:Position, endPosition:Position):
         self.grid = grid
-        self.startPosition = self.grid.findFirst(Grid.START)
-        self.endPosition = self.grid.findFirst(Grid.END)
+        self.startPosition = startPosition
+        self.endPosition = endPosition
         self.state = GridState(self.startPosition)
 
     def isWinState(self):
-        return self.state.playerPos == self.endPosition
+        return self.state.pos == self.endPosition
 
     def isValidMove(self, dir:Direction):
-        next = self.state.playerPos.move(dir)
+        next = self.state.pos.move(dir)
         return self.grid.validPos(next) and not self.grid.isWall(next)
 
     def getAllPossibleActions(self) -> List[Action]:
@@ -125,7 +126,20 @@ class GridEnvironment(Environment):
 
     def getNewState(self, action:GridAction) -> GridState:
         if not self.isValidMove(action.dir):
-            raise Exception(f"Invalid action {action.dir} from {self.state.playerPos}")
+            raise Exception(f"Invalid action {action.dir} from {self.state.pos}")
 
-        newPos = self.state.playerPos.move(action.dir)
+        newPos = self.state.pos.move(action.dir)
         return GridState(newPos)
+
+DEFAULT_MAZE = [[0,0,0,1,0,0,1,0],
+                [0,1,1,1,0,0,0,0],
+                [0,1,0,1,0,1,1,1],
+                [0,0,0,1,0,0,1,0],
+                [0,1,1,1,1,0,1,0],
+                [0,0,0,1,0,0,1,0],
+                [0,1,0,0,0,1,1,0],
+                [0,1,0,1,0,0,0,0],
+                ]
+
+DEFAULT_START = Position(0,0)
+DEFAULT_END = Position(5, 0)
